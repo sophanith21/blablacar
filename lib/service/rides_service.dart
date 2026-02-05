9c027e41496f67b757f4c812bbeab035a3442852
Meas Sophanith <sophanithmeas91@gmail.com>
2026-02-05 14:49:22 +0700

    BLA-XXX Start Code

---
+++ lib/service/rides_service.dart
@@ -0,0 +1,28 @@
+import '../dummy_data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(Location departure) {
+    return [];
+  }
+
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(int requestedSeat) {
+    return [];
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return [];
+  }
+}
